d13aee158c650766f9ece614549dd7a30d030212
benjaml <benjaml@orange.fr>
2015-11-24 20:33:48 +0100

    Ajout du script de dégat sur la durée

---
+++ Assets/DamageOverTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOverTime : MonoBehaviour {
+
+	public int DamagePerTick;
+	public int numberOfTicks;
+	public float timeBetweenTick;
+	// Use this for initialization
+	void Start () {
+		Destroy (this, timeBetweenTick * (numberOfTicks+1));
+		InvokeRepeating ("sendDamage",timeBetweenTick, timeBetweenTick);
+	}
+	
+	void sendDamage()
+	{
+		GetComponent<Player> ().removeLife (DamagePerTick);
+	}
+}
