46865fe6c8db8f5023366395821b93cad024b985
thomasDubrulle <thomasdubrulle25@hotmail.fr>
2015-11-25 17:19:47 +0100

    +Gestion boost, +Bouclier "contre-attaque"

---
+++ Assets/scripts/skills/Effect/CounterAttackFixedProtection.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CounterAttackFixedProtection : FixedProtection {
+
+    //CounterAttack's damage.
+    public float damages;
+    //Damage type of the counterAttack.
+    public DamageType dType;
+
+    public override float reduce(float damage, DamageType dType, Player player) {
+        PlayerManager.getInstance().sendDamage(PlayerManager.getInstance().getOtherPlayer(player), damages, dType);
+        return base.reduce(damage, dType, player);
+    }
+}
