64eb65821de304ad3e82a43725dd45e65e4326e9
benjaml <benjaml@orange.fr>
2015-11-25 14:46:52 +0100

    Ajout du chargement des platform

--- Assets/scripts/Platform/plateform.cs
+++ Assets/scripts/Platform/plateform.cs
@@ -30,7 +30,23 @@ public class plateform : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (skill) 
+		{
+			Texture2D texture = new Texture2D(128, 128);
+			transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = texture;
+			Vector2 centerPixel = new Vector2(64,64);
+			for (int y = 0; y < texture.height; y++) {
+				for (int x = 0; x < texture.width; x++) {
+					Color color;
+					if(Vector2.Distance(centerPixel,new Vector2(x,y)) <= (skill.currentCharge/skill.chargeTime) * Vector2.Distance(centerPixel,new Vector2(0,0)))
+					  	color = Color.gray;
+					else
+						color = Color.white; 
+					texture.SetPixel(x, y, color);
+				}
+			}
+			texture.Apply();
+		}
 	}
 	public void setPlayerSkill(GameObject player)
 	{
