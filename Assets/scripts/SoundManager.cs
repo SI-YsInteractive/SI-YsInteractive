0b9ae3d0e14fbaf95fb6e3c425d4302e3ac49a86
thomasDubrulle <thomasdubrulle25@hotmail.fr>
2015-11-26 17:48:09 +0100

    Ajout soundManager

---
+++ Assets/scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    public static SoundManager instance = null;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    //Return the public instance of the manager.
+    public static SoundManager getInstance() {
+        if (instance == null) {
+            instance = GameObject.FindObjectOfType(typeof(SoundManager)) as SoundManager;
+
+            if (instance == null) {
+                GameObject go = new GameObject("SkillManager");
+                instance = go.AddComponent<SoundManager>();
+            }
+        }
+        return instance;
+    }
+
+    /**Play a sound*/
+    public void playSound(AudioClip audioClip, float volume) {
+        audioSource.PlayOneShot(audioClip, volume);
+    }
+}
