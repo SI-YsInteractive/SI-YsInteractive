c1240f06201bd9699aecbb51fb8a217ba50db3fa
benjaml <benjaml@orange.fr>
2015-11-27 14:15:26 +0100

    Correction des description et bloquage du boutton back

--- Assets/scripts/Interface/DeckSelectorManager.cs
+++ Assets/scripts/Interface/DeckSelectorManager.cs
@@ -18,6 +18,7 @@ public class DeckSelectorManager : MonoBehaviour {
 
 	public GameObject selectionPanel;
 	public GameObject buttonPrefab;
+	public GameObject backButton;
 	// Use this for initialization
 	void Start(){
 		buildChoices (skillList);
@@ -25,6 +26,8 @@ public class DeckSelectorManager : MonoBehaviour {
 			playerId = 1;
 		else
 			playerId = 2;
+
+		backButton.GetComponent<Button> ().interactable = false;
 	}
 
 
@@ -32,51 +35,53 @@ public class DeckSelectorManager : MonoBehaviour {
 	{
 		skills [currentSkill].GetComponent<skillPanelPosition> ().text.text = skill.ToString ();
 		currentSkill++;
+		backButton.GetComponent<Button> ().interactable = true;
 		PlayerPrefs.SetInt ("Player" + playerId + "Skill" + currentSkill,(int)skill);
 		string description = "";
 		switch (skill)
 		{
 		case SkillType.AttaqueRapide:
-			description = "inflige 4 point de dégats, charge en 2 secondes";
+			description = "inflige 4pts de dégats, charge=2s";
 			break;
 		case SkillType.AttaquePuissante:
-			description = "inflige 18 point de dégats, charge en 7 secondes";
+			description = "inflige 18pts de dégats, charge=7s";
 			break;
 		case SkillType.BouclierBasique:
-			description = "protège de 3 point de dégats, charge en 1 secondes, dure 5 secondes";
+			description = "protège de 3pts de dégats, dure 5s, charge=1s";
 			break;
 		case SkillType.BouclierFort:
-			description = "protège de 12 point de dégats, charge en 4 secondes, dure 8 secondes";
+			description = "protège de 12pts de dégats, dure 8 secondes, charge=4s";
 			break;
 		case SkillType.BouleDeFeu:
-			description = "inflige 6 point de dégats, charge en 6 secondes, passe au travers des protections";
+			description = "inflige 6pts de dégats, pénètre les protections, charge=6s";
 			break;
 		case SkillType.LanceDeFoudre:
-			description = "inflige 3 point de dégats et bloque une compétence aléatoire pendant 2 secondes, charge en 6 secondes, passe au travers des protections";
+			description = "inflige 3pts de dégats et bloque une compétence aléatoire pendant 2s, pénètre les protections, charge=6s";
 			break;
 		case SkillType.EclairDeGlace:
-			description = "inflige 1.5 point de dégats, charge en 6 secondes, ralenti l'adversaire de 50% pendant 6 seconde, passe au travers des protections";
+			description = "inflige 1,5pts de dégats, ralentit l'adversaire de 50% pendant 6s, pénètre les protections, charge=6s";
 			break;
 		case SkillType.CoupDeGriffe:
-			description = "inflige 2 point de dégats, plus 5 sur 10 secondes, charge en 2 secondes";
+			description = "inflige 2pts de dégats, plus 5pts sur 10s, charge=2s";
 			break;
 		case SkillType.Esquive:
-			description = "esquive les attaques physiques pendant 2 seconde, charge en 3 secondes";
+			description = "esquive toutes les attaques physiques pendant 2s, charge=3s";
 			break;
 		case SkillType.ContreAttaque:
-			description = "protège de 1 point de dégat et en renvoie 3 pendant 2 seconde, charge en 1 secondes";
+			description = "protège de 1pt de dégat et en renvoie 3 pendant 2s, charge=1s";
 			break;
 		case SkillType.CoupDeGrace:
-			description = "inflige 3 points de dégat, ou 6 sur un ennemi ralenti, ou sonné, charge en 2 secondes";
+			description = "inflige 3pts de dégat, dommages x2 si l'ennemi est ralenti/sonné, charge=2s";
 			break;
 		case SkillType.ToileProtectrice:
-			description = "protège de 3 point de dégats, ralenti de 50% pendant 3 secondes l'adversaire si il attaque, dure 6 secondes, charge en 3 secondes";
+			description = "protège 3pts de dégats, ralentit de 50% pendant 3s l'adversaire si il attaque, durée=6s, charge=3s";
 			break;
 		case SkillType.PoisonParalysant:
-			description = "sonne l'adversaire pendant 6 secondes, charge en 6 secondes";
+			description = "paralyse l'adversaire pendant 6s, charge=6s";
 			break;
 		}
 		skills [currentSkill-1].GetComponent<skillPanelPosition> ().description.text = description;
+		skills [currentSkill-1].GetComponent<skillPanelPosition> ().description.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(skills [currentSkill-1].GetComponent<RectTransform> ().sizeDelta.x,150f);
 		skills [currentSkill-1].GetComponent<skillPanelPosition> ().skill = skill;
 		if(currentSkill == 5)
 		{
@@ -99,6 +104,8 @@ public class DeckSelectorManager : MonoBehaviour {
 				lastSlot.GetComponent<skillPanelPosition> ().text.text = "";
 				lastSlot.GetComponent<skillPanelPosition> ().description.text = "";
 				currentSkill--;
+				if(currentSkill == 0)
+					backButton.GetComponent<Button> ().interactable = false;
 				return;
 			}
 		}
